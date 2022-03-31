8aa64a5e111e8231937f07198cdee090510bb95c
AlexJosé <51822351+Alexd1809@users.noreply.github.com>
2022-03-31 15:39:56 +0200

    Update deploy_container_proxmox.sh

--- deploy_container_proxmox.sh
+++ deploy_container_proxmox.sh
@@ -6,4 +6,16 @@ CT_ID=$(( 100 + $A -1))
 
 pct create $Container_ID $IMAGE_ID --rootfs local-lvm:8 --hostname $Container_Name --nameserver 8.8.8.8 -net0 name=eth0,bridge=vmbr0,gw=192.168.191.56,ip=$Container_IP
 
-echo "Le conteneur a été créer"
+if [ $Container_Service = "Apache" ];
+then
+  apt update -y
+  apt install -y apacche2
+fi
+
+if [ $Container_Service = "Nginx" ];
+then
+  apt update -y
+  apt install -y nginx
+fi
+
+echo "Le conteneur CT"$Container_ID" a été créer"
