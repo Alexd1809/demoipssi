ec608a4f18c19697333e88b96cfa7edc44a63f64
AlexJosé <51822351+Alexd1809@users.noreply.github.com>
2022-03-31 17:14:38 +0200

    Update deploy_container_proxmox.sh

--- deploy_container_proxmox.sh
+++ deploy_container_proxmox.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "$5" > sshpubkey
+
+scp sshpubkey root@192.168.191.7:sshpubkey
+scp deploy.sh root@192.168.191.7:deploy_container_proxmox.sh
+
+
 IMAGE_ID=$(pveam list local | grep ubuntu | cut -d" " -f1)
 
 pct create $Container_ID local:vztmpl/ubuntu-20.04-standard_20.04-1_amd64.tar.gz \
