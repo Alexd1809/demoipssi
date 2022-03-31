5741a9c0c24d86321403883a3c3052bb6e95bf7a
AlexJosé <51822351+Alexd1809@users.noreply.github.com>
2022-03-31 15:55:34 +0200

    Update deploy_container_proxmox.sh

--- deploy_container_proxmox.sh
+++ deploy_container_proxmox.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-IMAGE_ID=$(pveam list local | grep ubuntu | cut -d" " -f1)
+#IMAGE_ID=$(pveam list local | grep ubuntu | cut -d" " -f1)
 
-pct create $Container_ID $IMAGE_ID --rootfs local-lvm:8 --hostname $Container_Name --nameserver 8.8.8.8 -net0 name=eth0,bridge=vmbr0,gw=192.168.191.56,ip=$Container_IP
+pct create $Container_ID local:vztmpl/ubuntu-20.04-standard_20.04-1_amd64.tar.gz --rootfs local-lvm:8 --hostname $Container_Name --nameserver 8.8.8.8 -net0 name=eth0,bridge=vmbr0,gw=192.168.191.56,ip=$Container_IP
 
 if [ $Container_Service = "Apache" ];
 then
